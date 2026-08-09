86bc194897e296f5efa3d2965a531d9ee524479c
lovesh semwal <loveshsemwal289@gmail.com>
2026-08-09 15:05:53 +0530

    Changing Tailwind css

--- client/src/components/EmergencyCard.jsx
+++ client/src/components/EmergencyCard.jsx
@@ -69,7 +69,7 @@ const EmergencyCard = ({
         </div>
 
         {/* Status */}
-        <div className="mb-5">
+        <div className="mb-5!">
           <span
             className={`px-4! py-2! rounded-full text-sm font-semibold ${
               statusColor[status]
